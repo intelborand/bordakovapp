83c430b81dba2158bbe2080189ac4da324775029
Andrzej <intelborand@gmail.com>
2023-10-23 17:59:10 +0200

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,27 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import '../styles/header.scss';
-import {Link} from 'react-router-dom';
 
 export default function Header() {
 
 
-    return(
+    return (
         <>
-        <header>
-            <h3 className='link'>
-                <Link to='/'>Andrii Bordakov</Link>
-            </h3>
-            <nav>
-                <ul>
-                    {/* <li>
+            <header>
+                <h3 className='link'>
+                    <Link to='/'>Andrii Bordakov</Link>
+                </h3>
+                <nav>
+                    <ul>
+                        {/* <li>
                         <Link to='/resume'>Resume</Link>
                     </li> */}
-                    <li>
-                        <Link to='/workshop'>Workshop</Link>
-                    </li>
-                </ul>
-            </nav>
-            {/* <div className='btn'><Link to='/'>Contact</Link></div> */}
-        </header>
+                        <li>
+                            <Link to='/workshop'>Workshop</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </>
     )
 };
